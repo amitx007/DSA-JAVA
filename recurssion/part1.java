9b80a21e306af4c4610adcbedf3d9a13bde02129
Amit Mohapatra <amitmohapatra02@gmail.com>
2021-10-06 12:32:27 +0530

    recurssion modified

--- recurssion/part1.java
+++ recurssion/part1.java
@@ -2,6 +2,25 @@ package recurssion;
 
 public class part1 {
     public static void main(String[] args) {
-
+        meth1(1);
+    }
+    public static void meth1(int n){
+        System.out.println(n);
+        meth2(n+1);
+    }
+    public static void meth2(int n){
+        System.out.println(n);
+        meth3(n+1);
+    }
+    public static void meth3(int n){
+        System.out.println(n);
+        meth4(n+1);
+    }
+    public static void meth4(int n){
+        System.out.println(n);
+        meth5(n+1);
+    }
+    public static void meth5(int n){
+        System.out.println(n);
     }
 }
